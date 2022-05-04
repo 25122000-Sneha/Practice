42af928239e5bc8d03b5d1e093b60ae8dd901541
25122000-Sneha <duttasneha25122000@gmail.com>
2022-05-05 00:17:39 +0530

    added few files

---
+++ PrepinstaTop100/PowerOfNumber/Power.java
@@ -0,0 +1,16 @@
+package PowerOfNumber;
+import java.util.*;
+public class Power {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter base: ");
+
+        int base = reader.nextInt();
+        System.out.println("Enter exponent: ");
+        int exponent = reader.nextInt();
+        int power = (int)Math.pow(base, exponent);
+        System.out.println(base+" to the power "+exponent+" is "+power);
+        reader.close();
+    }
+
+}
